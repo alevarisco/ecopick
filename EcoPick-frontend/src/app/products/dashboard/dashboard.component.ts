a2851be049fbd3a21ba61dc1a94dfc3e3ef17152
Alessandra Varisco <alessandravariscod3@gmail.com>
2021-02-01 19:50:57 -0400

    Dashboard
    NavBar y otros

--- EcoPick-frontend/src/app/products/dashboard/dashboard.component.ts
+++ EcoPick-frontend/src/app/products/dashboard/dashboard.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService, SelectItem} from 'primeng/api';
+import { MenuItem } from 'primeng/api';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  providers: [MessageService]
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  isNatural: boolean;
+  isEmpresarial: boolean;
+
+  constructor(private router: Router,
+    private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.isNatural = false;
   }
 
+  // showError() {
+  //   this.messageService.add({severity:'error', summary: 'Error', detail: 'Faltan campos requeridos'});
+  // }
+
+  sendPublicaciones(): void{
+    // this.router.navigate(['']);
+  }
+
+  sendMisPublicacioness(): void{
+    // this.router.navigate(['']);
+  }
+
+  sendExplorarProd(): void{
+    // this.router.navigate(['']);
+  }
+
+  sendMisPedidos(): void{
+    // this.router.navigate(['']);
+  }
 }
--- EcoPick-frontend/src/app/products/products-routing.module.ts
+++ EcoPick-frontend/src/app/products/products-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {GuestGuard} from '../core/guards/guest.guard';
+import {AuthorizedGuard} from '../core/guards/authorized.guard';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
-    canActivate: [GuestGuard]
+    // canActivate: [GuestGuard]
+    canActivate: [AuthorizedGuard]
   }
 ];
 
